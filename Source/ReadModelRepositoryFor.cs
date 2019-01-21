a7dd7622a53b1967fa09b787cd8065c1cdb63674
Michael Smith <michael@dolittle.com>
2019-01-21 10:04:00 +0100

    Remove “Read.” part of the namespace if that is present.

--- Source/ReadModelRepositoryFor.cs
+++ Source/ReadModelRepositoryFor.cs
@@ -16,7 +16,7 @@ namespace Dolittle.ReadModels.MongoDB
     /// </summary>
     public class ReadModelRepositoryFor<T> : IReadModelRepositoryFor<T> where T : Dolittle.ReadModels.IReadModel
     {
-        readonly string _collectionName = typeof(T).FullName;
+        readonly string _collectionName = ReplaceReadNamespace(typeof(T).FullName);
         readonly Configuration _configuration;
         readonly IMongoCollection<T> _collection;
 
@@ -60,6 +60,11 @@ namespace Dolittle.ReadModels.MongoDB
 
             var filter = Builders<T>.Filter.Eq("_id", id);
             _collection.ReplaceOne(filter, readModel, new UpdateOptions() { IsUpsert = true });
+        }
+
+	    static string RemoveReadNamespace(string source)
+        {	
+            return source.StartsWith("Read.") ? source.Substring(5) : source;
         }
    }
 }
