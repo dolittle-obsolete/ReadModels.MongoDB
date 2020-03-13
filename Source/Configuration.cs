476bb8cb2d812e49e3372f01b9f59b4bd75921af
Are Almaas <mralmaas@gmail.com>
2020-03-13 14:53:40 +0100

    add missing using statement

--- Source/Configuration.cs
+++ Source/Configuration.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Dolittle.ResourceTypes.Configuration;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 
